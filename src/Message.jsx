b64256492b50bba04667042d9df8da24f65a66f2
elgemp4 <marquegniesemilien@gmail.com>
2024-11-06 19:17:19 +0100

    Replaced @ by full name

--- src/Message.jsx
+++ src/Message.jsx
@@ -7,13 +7,16 @@ export default function Message({message, me, senders}) {
     }
 
     const isTargetedAtMe = () => {
-        return message.message.toLowerCase().indexOf(`@${me.sender.toLowerCase()}`) != -1;
+        const searched = `@${me.sender.toLowerCase()}`;
+        
+        return message.message.toLowerCase().indexOf(searched) != -1
+
     }
     
     return <>
         <div key={message._id} className={(me._id == message.sender._id ? "flex flex-col items-end " : " ") + (isTargetedAtMe() ? "bg-yellow-300" : " ")}>
               <h3 className='font-bold' dangerouslySetInnerHTML={{__html: getName()}}></h3>
-              <p dangerouslySetInnerHTML={{__html: message.message}}></p>
+              <p dangerouslySetInnerHTML={{__html: message.message.replace(`@${me.sender.toLowerCase()}`, me.sender)}}></p>
               <hr className='w-full'></hr>
         </div>
     </>
